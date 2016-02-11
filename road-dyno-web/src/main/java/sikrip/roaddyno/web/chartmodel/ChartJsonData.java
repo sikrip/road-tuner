7a5785ab6a34de3e34c30ec381fc6ce5fa9941e3
sikrip <sikrip@gmail.com>
2016-02-11 23:41:39 +0200

    working on plot generation

---
+++ road-dyno-web/src/main/java/sikrip/roaddyno/web/chartmodel/ChartJsonData.java
@@ -0,0 +1,135 @@
+package sikrip.roaddyno.web.chartmodel;
+
+import sikrip.roaddyno.model.DynoSimulationEntry;
+import sikrip.roaddyno.model.DynoSimulationResult;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChartJsonData {
+
+    public static final String TORQUE_COLUMN = "column-2";
+    public static final String POWER_COLUMN = "column-1";
+    public static final String RPM_AXIS = "rpm";
+
+    private Map<String, Object> root = new HashMap<>();
+    private DecimalFormat df = df = new DecimalFormat("#.0");
+
+    public ChartJsonData() {
+        df.setRoundingMode(RoundingMode.DOWN);
+    }
+
+    public Map<String, Object> createJsonData(DynoSimulationResult dynoResult) {
+        root.clear();
+
+        root.put("type", "serial");
+        root.put("categoryField", RPM_AXIS);
+        root.put("startDuration", 1);
+
+        Map<String, Object> categoryAxis = new HashMap<>();
+        categoryAxis.put("gridPosition", "start");
+        categoryAxis.put("labelFrequency", 3);
+        root.put("categoryAxis", categoryAxis);
+        root.put("trendLines", new ArrayList<>());
+
+        createGraphDefinitions(dynoResult.getName(), dynoResult.maxPower(), dynoResult.maxTorque());
+
+        root.put("guides", new ArrayList<>());
+
+        createValueAxisDefinition();
+
+        root.put("allLabels", new ArrayList<>());
+        root.put("balloon", new HashMap<>());
+
+        createLegendDefinition();
+
+        createTitlesDefinition();
+
+        createDataDefinition(dynoResult);
+
+        return root;
+    }
+
+    private void createDataDefinition(DynoSimulationResult dynoResult) {
+        List<Map<String, Object>> dataProvider = new ArrayList<>();
+
+
+        double[][] powerDataset = dynoResult.powerDataset();
+        double[][] torqueDataset = dynoResult.torqueDataset();
+
+        for (int i = 0; i < powerDataset[0].length; i++) {
+
+            Map<String, Object> dataEntry = new HashMap<>();
+            int rpm = (int) powerDataset[0][i];
+            String power = df.format(powerDataset[1][i]);
+            String torque = df.format(torqueDataset[1][i]);
+
+            dataEntry.put(RPM_AXIS, rpm);
+            dataEntry.put(POWER_COLUMN, power);
+            dataEntry.put(TORQUE_COLUMN, torque);
+            dataProvider.add(dataEntry);
+        }
+
+        root.put("dataProvider", dataProvider);
+    }
+
+    private void createTitlesDefinition() {
+        List<Map<String, Object>> titles = new ArrayList<>();
+        Map<String, Object> title = new HashMap<>();
+        title.put("id", "Title-1");
+        title.put("size", 15);
+        title.put("text", "Dyno Plot");
+        titles.add(title);
+        root.put("titles", titles);
+    }
+
+    private void createLegendDefinition() {
+        Map<String, Boolean> legend = new HashMap<>();
+        legend.put("enabled", true);
+        legend.put("useGraphSettings", true);
+        root.put("legend", legend);
+    }
+
+    private void createValueAxisDefinition() {
+        Map<String, String> valueAxis = new HashMap<>();
+        valueAxis.put("id", "ValueAxis-1");
+        valueAxis.put("title", "Hp | lb/ft");
+        List<Map<String, String>> valueAxes = new ArrayList<>();
+        valueAxes.add(valueAxis);
+        root.put("valueAxes", valueAxes);
+    }
+
+    private void createGraphDefinitions(String name, DynoSimulationEntry maxPower, DynoSimulationEntry maxTorque) {
+
+        List<Map<String, Object>> graphs = new ArrayList<>();
+        Map<String, Object> graph = new HashMap<>();
+        graph.put("balloonText", "[[title]] [[value]] @[[category]]");
+        //graph.put("bullet", "round");
+        //graph.put("bulletSize", 3);
+        graph.put("id", "AmGraph-1");
+        graph.put("lineColor", "#FF0000");
+        graph.put("lineThickness", 3);
+        graph.put("title", name + " power " + df.format(maxPower.getPower()) + " hp @" + (int)maxPower.getRpm());
+        graph.put("type", "smoothedLine");
+        graph.put("valueField", POWER_COLUMN);
+        graphs.add(graph);
+
+        graph = new HashMap<>();
+        graph.put("balloonText", "[[title]] [[value]] @[[category]]");
+        //graph.put("bullet", "square");
+        graph.put("id", "AmGraph-2");
+        graph.put("dashLength", 4);
+        graph.put("lineColor", "#FF0000");
+        graph.put("lineThickness", 2);
+        graph.put("title", name + " torque " + df.format(maxTorque.getTorque()) + " lb/ft @" + (int)maxTorque.getRpm());
+        graph.put("type", "smoothedLine");
+        graph.put("valueField", TORQUE_COLUMN);
+        graphs.add(graph);
+        root.put("graphs", graphs);
+    }
+
+}
--- road-dyno-web/src/main/java/sikrip/roaddyno/web/controller/HomeController.java
+++ road-dyno-web/src/main/java/sikrip/roaddyno/web/controller/HomeController.java
@@ -1,14 +1,19 @@
 package sikrip.roaddyno.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sikrip.roaddyno.engine.DynoSimulator;
+import sikrip.roaddyno.logreader.EcuLogReader;
+import sikrip.roaddyno.logreader.MegasquirtLogReader;
+import sikrip.roaddyno.model.DynoSimulationResult;
+import sikrip.roaddyno.model.LogEntry;
+import sikrip.roaddyno.web.chartmodel.ChartJsonData;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import sikrip.roaddyno.web.chartmodel.ChartModel;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -18,13 +23,16 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public String index(Model model) {
-		String chartJson="{}";
+		String chartDef;
+		EcuLogReader msLogReader = new MegasquirtLogReader();
 		try {
-			chartJson = objectMapper.writeValueAsString(new ChartModel());
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+			List<LogEntry> logEntries = msLogReader.readLog("/home/sikrip/Downloads/2016-01-18_22.30.22.msl", 96);
+			DynoSimulationResult result = DynoSimulator.run(logEntries, "AW11 Rotrex", 4.312, 1.310, 528, 905, 85, 1.8, 0.34);
+			chartDef = objectMapper.writeValueAsString(new ChartJsonData().createJsonData(result));
+		} catch (IOException e) {
+			chartDef="{}";
 		}
-		model.addAttribute("chartJson", chartJson);
+		model.addAttribute("chartDef", chartDef);
 		return "index";
 	}
 
