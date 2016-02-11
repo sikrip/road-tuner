7676d1cc19c478830df978834255a4686484b6d5
george.sikalias <george.sikalias@upstreamsystems.com>
2016-02-11 16:22:18 +0200

    initial work on creating the chart json

--- road-dyno-web/src/main/java/sikrip/roaddyno/web/controller/HomeController.java
+++ road-dyno-web/src/main/java/sikrip/roaddyno/web/controller/HomeController.java
@@ -1,13 +1,31 @@
 package sikrip.roaddyno.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import sikrip.roaddyno.web.chartmodel.ChartModel;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		String chartJson="{}";
+		try {
+			chartJson = objectMapper.writeValueAsString(new ChartModel());
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("chartJson", chartJson);
 		return "index";
 	}
+
 }
