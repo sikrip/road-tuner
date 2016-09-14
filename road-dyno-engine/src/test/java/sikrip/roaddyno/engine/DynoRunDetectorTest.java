5e8aca196616a096d41b9ed52f571ccd0ae170e0
george.sikalias <george.sikalias@upstreamsystems.com>
2016-09-14 17:08:28 +0300

    Unit testing for acceleration run detection without TPS via sample acceleration files .

--- road-dyno-engine/src/test/java/sikrip/roaddyno/engine/DynoRunDetectorTest.java
+++ road-dyno-engine/src/test/java/sikrip/roaddyno/engine/DynoRunDetectorTest.java
@@ -1,18 +1,15 @@
 package sikrip.roaddyno.engine;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import static junit.framework.TestCase.assertTrue;
 
-import org.junit.Ignore;
+import java.net.URL;
+import java.util.List;
+
 import org.junit.Test;
 
 import sikrip.roaddyno.eculogreader.EcuLogReader;
 import sikrip.roaddyno.eculogreader.MegasquirtLogReader;
 import sikrip.roaddyno.model.LogEntry;
-import sikrip.roaddyno.model.LogValue;
 
 /**
  * Tests for {@link DynoRunDetector}.
@@ -20,46 +17,23 @@ import sikrip.roaddyno.model.LogValue;
 public class DynoRunDetectorTest {
 
 	@Test
-	@Ignore
-	public void validateDynoRunStartAndFinish_noDeceleration() {
-
-		List<LogEntry> logEntries = new ArrayList<>();
-
-		for (int i = 0; i < 50; i++) {
-			Map<String, LogValue<Double>> values = new HashMap<>();
-			values.put("Time", new LogValue<>(100.0 + (i / 10.0), "sec"));
-			values.put("RPM", new LogValue<>(3000.0, "RPM"));
-			values.put("TPS", new LogValue<>(20.0, "%"));
-			new LogEntry(values, "Time", "RPM", "TPS");
-
-			logEntries.add(new LogEntry(values, "Time", "RPM", "TPS"));
-		}
-
-		for (double i = 0; i < 2000; i++) {
-			Map<String, LogValue<Double>> values = new HashMap<>();
-			values.put("Time", new LogValue<>(100.0 + (i / 10.0), "sec"));
-			values.put("RPM", new LogValue<>(3000.0 + (i * 2), "RPM"));
-			values.put("TPS", new LogValue<>(i < 4 ? 80.0 : 100.0, "%"));
-			new LogEntry(values, "Time", "RPM", "TPS");
-
-			logEntries.add(new LogEntry(values, "Time", "RPM", "TPS"));
-		}
-
-		DynoRunDetector.getAccelerationBounds(logEntries);
-	}
-
-	@Test
-	public void verifySampleMSLFile() throws Exception {
+	public void verifySampleMSLFiles() throws Exception {
 
 		EcuLogReader logReader = new MegasquirtLogReader();
-		//List<LogEntry> logEntries = logReader.readLog(getTestResourceUrl("/sample-dyno-run.msl").getPath(), 0);
-		List<LogEntry> logEntries = logReader.readLog(getTestResourceUrl("/sample-dyno-run-1.msl").getPath(), 0);
 
-		AccelerationBounds accelerationBounds = DynoRunDetector.getAccelerationBounds(logEntries).get(0	);
+		List<LogEntry> logEntries = logReader.readLog(getTestResourceUrl("/sample-dyno-run.msl").getPath(), 0);
+		AccelerationBounds accelerationBounds = DynoRunDetector.getAccelerationBounds(logEntries).get(0);
 
-		System.out.println(accelerationBounds);
-		System.out.println("Start: " + logEntries.get(accelerationBounds.getStart()));
-		System.out.println("End: " + logEntries.get(accelerationBounds.getEnd()));
+		//System.out.println("sample-dyno-run.msl " + accelerationBounds);
+		assertTrue(accelerationBounds.getStart() > 470);
+		assertTrue(accelerationBounds.getStart() < 615);
+
+		logEntries = logReader.readLog(getTestResourceUrl("/sample-dyno-run-1.msl").getPath(), 0);
+		accelerationBounds = DynoRunDetector.getAccelerationBounds(logEntries).get(0);
+
+		//System.out.println("sample-dyno-run-1.msl " + accelerationBounds);
+		assertTrue(accelerationBounds.getStart() > 680);
+		assertTrue(accelerationBounds.getStart() < 800);
 	}
 
 	public static URL getTestResourceUrl(String filename) {
