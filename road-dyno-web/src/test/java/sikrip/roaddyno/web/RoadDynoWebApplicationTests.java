25896b5719ca6aeb528a27207449b48b6873e345
george.sikalias <george.sikalias@upstreamsystems.com>
2016-10-04 15:44:08 +0300

    Fixed application config name on tests.

--- road-dyno-web/src/test/java/sikrip/roaddyno/web/RoadDynoWebApplicationTests.java
+++ road-dyno-web/src/test/java/sikrip/roaddyno/web/RoadDynoWebApplicationTests.java
@@ -2,6 +2,7 @@ package sikrip.roaddyno.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,6 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RoadDynoWebApplication.class)
 @WebAppConfiguration
+@TestPropertySource(properties = {
+		"spring.config.name = road.dyno.application"})
 public class RoadDynoWebApplicationTests {
 
 	@Test
