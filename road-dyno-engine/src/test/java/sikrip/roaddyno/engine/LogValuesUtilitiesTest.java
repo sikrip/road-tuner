5fa3a04377c0f954b90d5fdc4c646432bcc8efaf
gsikalias <gsikalias@eurobank.gr>
2018-08-08 17:20:24 +0300

    Tests ib log value utils.

---
+++ road-dyno-engine/src/test/java/sikrip/roaddyno/engine/LogValuesUtilitiesTest.java
@@ -0,0 +1,13 @@
+package sikrip.roaddyno.engine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogValuesUtilitiesTest {
+
+    @Test
+    public void verifySmoothVelocity() {
+
+    }
+}
