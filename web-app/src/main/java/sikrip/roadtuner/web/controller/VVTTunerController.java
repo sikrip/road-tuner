801a2530a113b06cbd81a648a232acfdc9dd3ede
gsikalias <gsikalias@eurobank.gr>
2018-10-11 10:58:57 +0300

    Imports fix

--- web-app/src/main/java/sikrip/roadtuner/web/controller/VVTTunerController.java
+++ web-app/src/main/java/sikrip/roadtuner/web/controller/VVTTunerController.java
@@ -3,7 +3,6 @@ package sikrip.roadtuner.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
