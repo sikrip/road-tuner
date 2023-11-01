2670f29533968e53266a437be14f9405aa346a57
sikrip <sikrip@gmail.com>
2023-11-01 14:26:54 +0200

    First version of WOT tuner.

--- engine/src/main/java/sikrip/roadtuner/engine/wottuner/WotTuner.java
+++ engine/src/main/java/sikrip/roadtuner/engine/wottuner/WotTuner.java
@@ -1,14 +1,9 @@
 package sikrip.roadtuner.engine.wottuner;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import sikrip.roadtuner.model.LogEntry;
+import sikrip.roadtuner.model.WotTuneResult;
 import sikrip.roadtuner.model.WotTunerProperties;
 
 /**
@@ -17,9 +12,8 @@ import sikrip.roadtuner.model.WotTunerProperties;
 public class WotTuner {
 
     private static final String DECIMAL_FORMAT = "%6.3f";
-    private static final String NEW_FUEL_MAP_FILE = "./new-fuel.map";
 
-    public static String run(List<sikrip.roadtuner.model.LogEntry> logEntries, double[][] currentFuelMap, WotTunerProperties wotTunerProperties) throws IOException {
+    public static WotTuneResult analyze(List<sikrip.roadtuner.model.LogEntry> logEntries, double[][] currentFuelMap, WotTunerProperties wotTunerProperties) throws IOException {
         final double[][] loggedAfr = new double[wotTunerProperties.getFuelTableSize()][wotTunerProperties.getFuelTableSize()];
         final int[][] loggedAfrSample = new int[wotTunerProperties.getFuelTableSize()][wotTunerProperties.getFuelTableSize()];
         double wotStart = 0;
@@ -40,103 +34,33 @@ public class WotTuner {
             if (underWOT) {
                 if (logEntry.getTime().getValue() - wotStart >= wotTunerProperties.getAccelEnrichSeconds()) {
                     // fuel enrichment done
-                    int col = logEntry.get(wotTunerProperties.getRpmIdxHeader()).getValue().intValue();
                     int row = logEntry.get(wotTunerProperties.getLoadIdxHeader()).getValue().intValue();
-                    final double currentAvgSum =
-                        loggedAfr[col][row] * loggedAfrSample[col][row];
-                    loggedAfrSample[col][row]++;
-                    loggedAfr[col][row] =
-                        (currentAvgSum + logEntry.get(wotTunerProperties.getAfrHeader()).getValue()) /
-                            loggedAfrSample[col][row];
+                    int col = logEntry.get(wotTunerProperties.getRpmIdxHeader()).getValue().intValue();
+                    final double currentAvgSum = loggedAfr[row][col] * loggedAfrSample[row][col];
+                    loggedAfrSample[row][col]++;
+                    loggedAfr[row][col] = (currentAvgSum + logEntry.get(wotTunerProperties.getAfrHeader()).getValue()) / loggedAfrSample[row][col];
                 }
             }
-
         }
 
-        System.out.println("\n========= Logged AFR ===========");
-        for (int i = 0; i < loggedAfr.length; i++) {
-            if (i==0) {
-                printMapN_Values();
-            }
-            for (int j = 0; j < loggedAfr[i].length; j++) {
-                if (j==0) {
-                    printMapP_Value(i);
-                }
-                if (loggedAfrSample[i][j] >= minNumberOfSamples) {
-                    System.out.printf(DECIMAL_FORMAT, loggedAfr[i][j]);
-                } else {
-                    System.out.printf(DECIMAL_FORMAT, 0.0);
-                }
-                if (j < loggedAfr[i].length - 1) {
-                    System.out.print("\t");
-                }
-            }
-            System.out.println();
-        }
-        final double[][] newFuelMap = new double[fuelTableSize][fuelTableSize];
+        final double[][] newFuelMap = new double[wotTunerProperties.getFuelTableSize()][wotTunerProperties.getFuelTableSize()];
+        final double[][] fuelMapDiff = new double[wotTunerProperties.getFuelTableSize()][wotTunerProperties.getFuelTableSize()];
         for (int i = 0; i < loggedAfr.length; i++) {
             for (int j = 0; j < loggedAfr[i].length; j++) {
-                if (loggedAfrSample[i][j] >= minNumberOfSamples) {
-                    final double newFuelValue = (loggedAfr[i][j] / wotTargetAfr) * currentFuelMap[i][j];
+                if (loggedAfrSample[i][j] >= wotTunerProperties.getMinNumberOfSamples()) {
+                    final double newFuelValue = (loggedAfr[i][j] / wotTunerProperties.getWotTargetAfr()) * currentFuelMap[i][j];
                     newFuelMap[i][j] = newFuelValue;
                 } else {
                     newFuelMap[i][j] = currentFuelMap[i][j];
                 }
+                fuelMapDiff[i][j] = newFuelMap[i][j] - currentFuelMap[i][j];
             }
         }
 
-        System.out.printf("\n========= New Fuel Map (also saved under %s) ===========\n", NEW_FUEL_MAP_FILE);
-        try (BufferedWriter newFuelMapWriter = new BufferedWriter(new FileWriter(NEW_FUEL_MAP_FILE))) {
-            for (int i = 0; i < newFuelMap.length; i++) {
-                if (i==0) {
-                    printMapN_Values();
-                }
-                for (int j = 0; j < newFuelMap[i].length; j++) {
-                    if (j==0) {
-                        printMapP_Value(i);
-                    }
-                    System.out.printf(DECIMAL_FORMAT, newFuelMap[i][j]);
-                    newFuelMapWriter.write(String.format("%.3f", newFuelMap[i][j]));
-                    if (j < newFuelMap[i].length - 1) {
-                        System.out.print("\t");
-                        newFuelMapWriter.write("\t");
-                    }
-                }
-                System.out.println();
-                newFuelMapWriter.write("\n");
-            }
-        }
-        System.out.println("\n========= New-Old Map ===========");
-        for (int i = 0; i < newFuelMap.length; i++) {
-            if (i==0) {
-                printMapN_Values();
-            }
-            for (int j = 0; j < newFuelMap[i].length; j++) {
-                if (j==0) {
-                    printMapP_Value(i);
-                }
-                System.out.printf(DECIMAL_FORMAT, newFuelMap[i][j] - currentFuelMap[i][j]);
-                if (j < newFuelMap[i].length - 1) {
-                    System.out.print("\t");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    private static void printMapP_Value(int i) {
-        System.out.printf("%5d(%2d)", loadLabels[i], i + 1);
-    }
-
-    private static void printMapN_Values() {
-        // each MAPP is 9 chars long
-        System.out.print("         ");
-        for(int j = 0; j < fuelTableSize; j++) {
-            System.out.printf("%6d", rpmLabels[j]);
-            if (j < fuelTableSize -1) {
-                System.out.print("\t");
-            }
-        }
-        System.out.println();
+        return WotTuneResult.builder()
+            .loggedAFRTable(loggedAfr)
+            .newFuelMap(newFuelMap)
+            .fuelMapDiff(fuelMapDiff)
+            .build();
     }
 }
--- web-app/src/main/java/sikrip/roadtuner/web/controller/WotTunerController.java
+++ web-app/src/main/java/sikrip/roadtuner/web/controller/WotTunerController.java
@@ -5,25 +5,23 @@ import static sikrip.roadtuner.web.utils.ControllerUtils.showErrorPage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import sikrip.roadtuner.engine.wottuner.WotTuner;
 import sikrip.roadtuner.logreader.DatalogitLogReader;
 import sikrip.roadtuner.model.LogEntry;
+import sikrip.roadtuner.model.WotTuneResult;
 import sikrip.roadtuner.model.WotTunerProperties;
-import sikrip.roadtuner.web.model.VvtTuneOptions;
 
 @Controller
 @Scope("session")
@@ -33,54 +31,65 @@ public class WotTunerController {
     private final Logger LOGGER = LoggerFactory.getLogger(WotTunerController.class);
 
     private WotTunerProperties wotTunerProperties = new WotTunerProperties();
-    private double[][] fuelMap = null;
+    private WotTuneResult wotTuneResult = null;
+    private double[][] currentFuelMap = null;
+
     @Value("${spring.servlet.multipart.max-file-size}")
-    private String maxFileSize;
+    private String maxFileSize = null;
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("maxFileSize", maxFileSize);
+        model.addAttribute("nav", "wot-tuner");
+        model.addAttribute("currentFuelMap", currentFuelMap);
+        model.addAttribute("wotTunerProperties", wotTunerProperties);
+    }
 
     @RequestMapping
-    public String index(Model model) {
-        model.addAttribute("nav", "wot-tuner");
+    public String index() {
         return "wot-tuner";
     }
 
-    @RequestMapping("read-fuel-map")
+    @RequestMapping("/reset")
+    public String index(Model model) {
+        wotTunerProperties = new WotTunerProperties();
+        currentFuelMap = null;
+        model.addAttribute("currentFuelMap", currentFuelMap);
+        model.addAttribute("wotTunerProperties", wotTunerProperties);
+        return "wot-tuner";
+    }
+
+    @RequestMapping(value = "read-fuel-map", method = RequestMethod.POST)
     public String readFuelMap(Model model, @RequestParam("file") MultipartFile file) {
         try (final BufferedReader fuelMapReader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            fuelMap = new double[wotTunerProperties.getFuelTableSize()][wotTunerProperties.getFuelTableSize()];
+            currentFuelMap = new double[wotTunerProperties.getFuelTableSize()][wotTunerProperties.getFuelTableSize()];
             String line;
-            StringBuilder fuelMapStr = new StringBuilder();
+            int rowIdx = 0;
             while ((line = fuelMapReader.readLine()) != null) {
-                fuelMapStr.append(line).append("\n");
                 final String[] mapValues = line.split("\t");
-                for (int i = 0; i < mapValues.length; i++) {
-                    fuelMap[i][i] = Double.parseDouble(mapValues[i]);
+                for (int colIdx = 0; colIdx < mapValues.length; colIdx++) {
+                    currentFuelMap[rowIdx][colIdx] = Double.parseDouble(mapValues[colIdx]);
                 }
+                rowIdx++;
             }
-            model.addAttribute("fuelMapStr", fuelMapStr.toString());
         } catch (IOException e) {
-            System.err.println("Could not load fuel map");
+            currentFuelMap = null;
+            LOGGER.error("Could not load fuel map", e);
+        } finally {
+            model.addAttribute("currentFuelMap", currentFuelMap);
         }
         return "wot-tuner";
     }
 
-    @RequestMapping("/load-logs")
-    public String loadLogsForm(Model model) {
-        model.addAttribute("wotTunerProperties", wotTunerProperties);
-        model.addAttribute("maxFileSize", maxFileSize);
-        model.addAttribute("nav", "wot-tuner");
-
-        return "wot-tuner-form";
-    }
-
     @RequestMapping(value = "/run", method = RequestMethod.POST)
-    public String run(Model model, @RequestParam("file") MultipartFile file, VvtTuneOptions vvtTuneOptions) {
+    public String run(Model model, @RequestParam("file") MultipartFile file, WotTunerProperties wotTunerProperties) {
         try {
+            this.wotTunerProperties = wotTunerProperties;
             final DatalogitLogReader logReader = new DatalogitLogReader();
             final List<LogEntry> logEntries = logReader.readLog(file.getInputStream());
-
-            model.addAttribute("wotTuneResult", "ok");
-            model.addAttribute("nav", "wot-tuner");
-            return "wot-tuner-results";
+            wotTuneResult = WotTuner.analyze(logEntries, currentFuelMap, wotTunerProperties);
+            model.addAttribute("wotTuneResult", wotTuneResult);
+            return "wot-tuner";
         } catch (Exception e) {
             LOGGER.error("Could not run WOT tuner.", e);
             return showErrorPage(LOGGER, model, "Could not run WOT tuner.");
